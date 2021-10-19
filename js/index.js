815c1b7391b591a2ee90cf2b41ab47602a8f86ce
unknown <sayanroyal370@gmail.com>
2021-10-19 10:46:28 +0530

    final changes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,40 @@
+//render items 
+const render=()=>{
+
+  var arr = localStorage.getItem('testObject');
+
+  arr=JSON.parse(arr);
+
+  const myEle = document.getElementById("divRender");
+  myEle.innerHTML = '';
+
+  let divContent='';
+  document.getElementById("txtTaskCount").innerHTML=arr.length;
+  
+  if(arr.length>0){
+  arr.reverse().map(function(val, index){
+    divContent=divContent+`<div class="card-body item-shadow ${val.status=='1' ? `bg-light text-dark` : ``}"><div class="row"><div class="col-md-1 col-1"><input type="checkbox" class="checkboxes form-control" onclick="checkTask(this)" ${val.status=='1' ? `checked` : ``} value="${index}" id="txtCheck${index}"></div><div class="col-md-8 col-10">  <span class="font-bold ${val.status=='1' ? `checkboxTxts` : ``}" id="txtTaskname${index}">${val.name}</span></div><div class="col-md-3"><button class="btn btn-danger float-right" onClick="deleteItem(${index})"><i class="fa fa-trash"></i></button><button class="btn btn-success float-right" id="btnx${index}" onClick="editData(${index})"><i class="fa fa-edit"></i></button></div></div></div>`;
+          
+        });
+
+      }else{
+
+        divContent=`<div class="card-body"><div class="row"><div class="col-md-12"><h4 class="text-center">No Task Found</h4></div></div></div>`;
+    
+      }
+
+      
+        myEle.innerHTML=divContent;
+  
+}
 
 
+//add list items
 const addToDo=()=>{
 
   var txttodo=document.getElementById('txttodo').value;
+
+  if(txttodo!=null && txttodo!=''){
 
   document.getElementById('txttodo').value='';
 
@@ -37,37 +69,17 @@ localStorage.setItem('testObject', JSON.stringify(retrievedObject));
 
 render();
 
+return true;
+
+}else{
+
+  alert("Please Enter A Value To Continue");
+  return false;
+}
 
 }
 
-const render=()=>{
-
-  var arr = localStorage.getItem('testObject');
-
-  arr=JSON.parse(arr);
-
-  const myEle = document.getElementById("divRender");
-  myEle.innerHTML = '';
-
-  let divContent='';
-  document.getElementById("txtTaskCount").innerHTML=arr.length;
-  
-  if(arr.length>0){
-  arr.reverse().map(function(val, index){
-    divContent=divContent+`<div class="card-body ${val.status=='1' ? `bg-light text-dark` : ``}"><div class="row"><div class="col-md-1"><input type="checkbox" class="checkboxes form-control" onclick="checkTask(this)" ${val.status=='1' ? `checked` : ``} value="${index}" id="txtCheck${index}"></div><div class="col-md-8">  <span id="txtTaskname${index}">${val.name}</span></div><div class="col-md-3 "><button class="btn btn-danger float-right" onClick="deleteItem(${index})"><i class="fa fa-trash"></i></button><button class="btn btn-success float-right" id="btnx${index}" onClick="editData(${index})"><i class="fa fa-edit"></i></button></div></div></div>`;
-          
-        });
-
-      }else{
-
-        divContent=`<div class="card-body"><div class="row"><div class="col-md-12"><h4 class="text-center">No Task Found</h4></div></div></div>`;
-    
-      }
-
-      
-        myEle.innerHTML=divContent;
-  
-}
+//check box task
 
 const checkTask=(el)=>{
 
@@ -99,6 +111,8 @@ localStorage.setItem('testObject', JSON.stringify(updatedItems.reverse()));
 render();
 }
 
+//delete items
+
 const deleteItem=(elIndex)=>{
 
   
@@ -120,15 +134,57 @@ localStorage.setItem('testObject', JSON.stringify(arr.reverse()));
 render();
 }
 
+
+//edit items
+
 const editData=(elIndex)=>{
 
   const myData = document.getElementById(`txtTaskname${elIndex}`).innerText;
   document.getElementById(`txtTaskname${elIndex}`).innerHTML='';
-  document.getElementById(`txtTaskname${elIndex}`).innerHTML=`<input type="text" class="form-control" placeholder="Enter Todo Task" id="txtEditInp${elIndex}" value="${myData}">`
+  document.getElementById(`txtTaskname${elIndex}`).innerHTML=`<form onSubmit="return updateValue(${elIndex});"><input type="text" class="form-control w-100" placeholder="Enter Todo Task" required id="txtEditInp${elIndex}" value="${myData}"></form>`
 document.getElementById(`btnx${elIndex}`).innerHTML='Update';
 
 document.getElementById(`btnx${elIndex}`).setAttribute("onclick", `updateValue(${elIndex})`);
+document.getElementById("txtTaskname"+elIndex).classList.add("cancelstrikethrough");
 }
+
+//update value of current edited task
+const updateValue=(elIndex)=>{
+
+  
+
+  var eleVal=document.getElementById("txtEditInp"+elIndex).value;
+
+  if(eleVal!=null && eleVal!=''){
+  var arr = localStorage.getItem('testObject');
+ 
+
+arr=JSON.parse(arr);
+
+
+
+updatedItems=arr.reverse();
+
+updatedItems[elIndex].name=eleVal;
+
+localStorage.setItem('testObject', JSON.stringify(updatedItems.reverse()));
+document.getElementById("txtTaskname"+elIndex).classList.remove("cancelstrikethrough");
+
+document.getElementById('txtTaskname'+elIndex).innerHTML=eleVal;
+document.getElementById(`btnx${elIndex}`).innerHTML='<i class="fa fa-edit"></i>';
+
+document.getElementById(`btnx${elIndex}`).setAttribute("onclick", `editData(${elIndex})`);
+
+return true;
+}else{
+
+  alert("Please Enter A Value");
+  return false;
+}
+
+}
+
+//call render function
 
 render();
 
